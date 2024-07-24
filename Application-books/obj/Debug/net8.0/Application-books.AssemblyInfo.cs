ba951ca72b37002f05df1bf5ac0e10fdffa9c9cf
MichaelGald <michaelcom292@gmail.com>
2024-07-24 00:08:31 -0600

    union-rama-libro

--- Application-books/obj/Debug/net8.0/Application-books.AssemblyInfo.cs
+++ Application-books/obj/Debug/net8.0/Application-books.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application-books")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f72c639e90982b48970b74c03ed8f8350d79ca5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85f4872d076f6ea5b9da27971ced8593c678a1d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application-books")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application-books")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
