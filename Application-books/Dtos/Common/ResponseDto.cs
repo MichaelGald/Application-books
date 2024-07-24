641f03a0b136b332fc2397b9e6d2e4a14475d926
MichaelGald <michaelcom292@gmail.com>
2024-07-23 22:02:19 -0600

    response-creado

---
+++ Application-books/Dtos/Common/ResponseDto.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Application_books.Dtos.Common
+{
+    public class ResponseDto<T>
+    {
+        public T Data { get; set; }
+
+        public string Message { get; set; }
+
+        [JsonIgnore]
+        public int StatusCode { get; set; }
+
+        public bool Status { get; set; }
+    }
+}
--- Application-books/Services/LibrosServices.cs
+++ Application-books/Services/LibrosServices.cs
@@ -1,6 +1,39 @@
-﻿namespace Application_books.Services
+﻿using Application_books.Dtos.Common;
+using Application_books.Dtos.Libros;
+using Application_books.Services.Interface;
+
+namespace Application_books.Services
 {
-    public class LibrosServices
+    public class LibrosServices : ILibrosServices
     {
+        public readonly string _JSON_FILE;
+        public LibrosServices()
+        {
+            _JSON_FILE = "seedData/categories.json";
+        }
+        public Task<ResponseDto<LibroDto>> CreateAsync(LibroCreateDto dto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseDto<LibroDto>> DeleteAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseDto<LibroDto>> EditAsync(LibroEditDto dto, Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseDto<LibroDto>> GetLibroByAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseDto<List<LibroDto>>> GetLibroListAsync()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
