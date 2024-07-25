de78f949cec85ab417d739c067081b0018817485
MichaelGald <michaelcom292@gmail.com>
2024-07-25 02:10:55 -0600

    union-rama-automapper

--- Application-books/obj/Debug/net8.0/Application-books.AssemblyInfo.cs
+++ Application-books/obj/Debug/net8.0/Application-books.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application-books")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de24dee572026671b72f081e34c2c893391e7723")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7042bc2bb96d515ebc58bfdd9786bba7eba44ff0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application-books")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application-books")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
