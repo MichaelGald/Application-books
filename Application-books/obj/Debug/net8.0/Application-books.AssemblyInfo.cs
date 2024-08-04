db5c7aad4cf1e57b15881508d6d0c9ae3761a029
MichaelGald <michaelcom292@gmail.com>
2024-08-03 23:51:52 -0600

    termiando-crud-autor

--- Application-books/obj/Debug/net8.0/Application-books.AssemblyInfo.cs
+++ Application-books/obj/Debug/net8.0/Application-books.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application-books")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbdc88402864ae84174ce369ce3bc72b2732153d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+827f30a723f913530dcd820c6f106376f87da9a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application-books")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application-books")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
