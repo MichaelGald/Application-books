dce179d28893ee50d80cd6e9dbe3c342a40c6407
Jey-Arita <jomaarita@gmail.com>
2024-08-03 23:48:14 -0600

    uniendo ramas

--- Application-books/obj/Debug/net8.0/Application-books.AssemblyInfo.cs
+++ Application-books/obj/Debug/net8.0/Application-books.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application-books")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d13ffa7b187046591e6e6cafedf635f5007979db")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e554bdf291a4dcbfa3607bed7c87d6eb5fcf4b2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application-books")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application-books")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
