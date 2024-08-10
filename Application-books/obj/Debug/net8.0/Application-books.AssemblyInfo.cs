06c78e4994c33dab0bb0ecf451317d36bd0d3678
Jey-Arita <jomaarita@gmail.com>
2024-08-10 16:04:38 -0600

    dtos-favoritos

--- Application-books/obj/Debug/net8.0/Application-books.AssemblyInfo.cs
+++ Application-books/obj/Debug/net8.0/Application-books.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application-books")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91483c917a4178b4d88bd7f7815289c2f042b4ee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05a8ee66c43c0e33b97da97cfe17dc4345c9bd9c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application-books")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application-books")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
