397fecf3790198c9ffb79648f3a1ac75740121c0
Jey-Arita <jomaarita@gmail.com>
2024-08-11 21:58:43 -0600

    creando-frontend

--- Application-books/obj/Debug/net8.0/Application-books.AssemblyInfo.cs
+++ Application-books/obj/Debug/net8.0/Application-books.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application-books")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1deba8e8ab1368df2832ce50d9eef3d107d4180c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+214bad9c4c09ac3859ca05d2de4938d77a9c2daa")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application-books")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application-books")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
