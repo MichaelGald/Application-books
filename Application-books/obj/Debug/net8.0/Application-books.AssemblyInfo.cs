54ad3c97facabbc518cc43602789e6eac66e1da2
MichaelGald <michaelcom292@gmail.com>
2024-08-18 19:24:39 -0600

    guardando-1

--- Application-books/obj/Debug/net8.0/Application-books.AssemblyInfo.cs
+++ Application-books/obj/Debug/net8.0/Application-books.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application-books")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7887dc9240d90f041fd601c69e3488a46513eb71")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20a54c3fcb7bb5ddaf28687fa45524d4e83dee00")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application-books")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application-books")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
